a8a19bf9e1ad92b5fbce720e2f7c9117d908d9fd
Dima <dimmaa9@gmail.com>
2021-07-14 23:52:07 +0300

    1

--- public/js/rozetka.js
+++ public/js/rozetka.js
@@ -1,6 +1,23 @@
 $(window).load(function () {
     $('.loader').hide();
+    //$('#divTable').show();
+    let div = document.getElementById('divTable');
+    let divDisplay = getComputedStyle(div).display;
+    if (divDisplay == 'none') {
+        div.style.display = 'block';
+    }
+});
+
+$(document).ajaxStart(function () {
+    $('.divTable').hide();
+    $('.loader').show();
+}).ajaxStop(function () {
+    $('.loader').hide();
     $('.divTable').show();
+});
+
+$(document).ready(function () {
+    console.log('start')
     $.get('/rozetka-data', function (arr) {
         console.log('start2')
         let table = $("#table").DataTable();
@@ -13,25 +30,14 @@ $(window).load(function () {
                 {data: 'feed_id'},
                 {data: 'product_name'},
                 {data: 'price'},
-                {data: 'url',
+                {
+                    data: 'url',
                     "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-                        $(nTd).html("<a href='"+oData.url+"'  target=\"_blank\">"+oData.url+"</a>");
-                    }},
+                        $(nTd).html("<a href='" + oData.url + "'  target=\"_blank\">" + oData.url + "</a>");
+                    }
+                },
                 {data: 'status'}
             ]
         });
     });
 });
-
-$(document).ajaxStart(function () {
-    $('.divTable').hide();
-    $('.loader').show();
-}).ajaxStop(function () {
-    $('.loader').hide();
-    $('.divTable').show();
-});
-
-// $(document).ready(function () {
-//     console.log('start')
-//
-// });
