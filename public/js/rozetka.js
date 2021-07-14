c1d3cc7b32717a668dbae4eac1ee110a062ef5a6
Dima <dimmaa9@gmail.com>
2021-07-15 00:14:03 +0300

    1

--- public/js/rozetka.js
+++ public/js/rozetka.js
@@ -36,27 +36,7 @@ $(document).ready(function () {
                     }
                 },
                 {data: 'status'}
-            ],
-            initComplete: function () {
-                this.api().columns().every( function () {
-                    let column = this;
-                    let select = $('<select><option value=""></option></select>')
-                        .appendTo( $(column.footer()).empty() )
-                        .on( 'change', function () {
-                            let val = $.fn.dataTable.util.escapeRegex(
-                                $(this).val()
-                            );
-
-                            column
-                                .search( val ? '^'+val+'$' : '', true, false )
-                                .draw();
-                        } );
-
-                    column.data().unique().sort().each( function ( d, j ) {
-                        select.append( '<option value="'+d+'">'+d+'</option>' )
-                    } );
-                } );
-            }
+            ]
         });
     });
 });
