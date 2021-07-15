713e93897a5356df5ef44755ff2e6d64174ef43e
Dima <dimmaa9@gmail.com>
2021-07-15 18:24:44 +0300

    1

--- public/js/rozetka.js
+++ public/js/rozetka.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
             buttons: [
                 'searchPanes'
             ],
-            dom: 'Plfrtip'
+            dom: 'Bfrtip'
         });
     });
 });
